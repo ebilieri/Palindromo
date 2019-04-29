37071234ccbccc377dbefd9364fd74d47fd8eb18
Emerson <ebilieri@hotmail.com>
2019-04-29 15:11:21 -0300

    Add project files.

---
+++ Palindromo/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Palindromo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Digite uma palavra");
+            string palavra = Console.ReadLine();
+            Console.WriteLine(IsPalindromo(palavra));
+            Console.ReadKey();
+
+        }
+
+        private static bool IsPalindromo(string palavra)
+        {
+            palavra = palavra.Replace(" ", "");
+            string palindromo = string.Empty;
+            var chars = palavra.ToCharArray();
+            for (int i = chars.Length - 1; i >= 0; i--)
+            {
+                Console.Write("{0}", chars[i]);
+                palindromo += chars[i];
+            }
+
+            Console.WriteLine();
+            return palindromo.ToLower() == palavra.ToLower();
+        }
+    }
+}
